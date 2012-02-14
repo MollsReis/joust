cd3c2ea5b04fb26c1e196a2e789c2b50e6e92310
Scott Reis <scott.reis@vertive.com>
2012-02-14 16:06:25 -0600

    remove duplicate notification check

--- lib/joust/expected_response/version_20.rb
+++ lib/joust/expected_response/version_20.rb
@@ -3,8 +3,7 @@ class Joust
 
     def match?(json_hash)
       return false unless super
-      return true if json_hash.empty? && @expected.values == [:missing, :missing, :missing]
-      json_hash['jsonrpc'] == '2.0' && (!!json_hash['result'] ^ !!json_hash['error'])
+      (json_hash['jsonrpc'] == '2.0' && (!!json_hash['result'] ^ !!json_hash['error'])) || json_hash.empty?
     end
 
   end
