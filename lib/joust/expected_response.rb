81ee5c95c0e626584f1ffaba64702b5ec5dce088
Scott Reis <scott.reis@vertive.com>
2012-03-14 13:26:08 -0500

    remove unnecessary block for rescue

--- lib/joust/expected_response.rb
+++ lib/joust/expected_response.rb
@@ -2,20 +2,18 @@ class Joust
   class ExpectedResponse
 
     def match?(json_hash)
-      begin
-        ['result', 'error', 'id'].each { |key| @expected[key] = :missing if !@expected.has_key?(key) }
-        if json_hash.empty?
-          return false unless @expected.values == [:missing, :missing, :missing]
-          return true
-        end
-        @expected.each do |key, val|
-          next if val == :missing && !json_hash.has_key?(key)
-          return false unless json_hash[key] == val
-        end
-        true
+      ['result', 'error', 'id'].each { |key| @expected[key] = :missing if !@expected.has_key?(key) }
+      if json_hash.empty?
+        return false unless @expected.values == [:missing, :missing, :missing]
+        return true
+      end
+      @expected.each do |key, val|
+        next if val == :missing && !json_hash.has_key?(key)
+        return false unless json_hash[key] == val
+      end
+      true
       rescue Exception
         false
-      end
     end
 
   end
